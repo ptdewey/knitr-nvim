54003474c952156bd2e65116120fddd67d78acc9
Patrick <57921252+ptdewey@users.noreply.github.com>
2024-02-11 12:29:26 -0500

    Show error messages on failed knit (#2)
    Updated output to be more verbose upon failed knit.

--- lua/knitr.lua
+++ lua/knitr.lua
@@ -2,72 +2,90 @@ local K = {}
 
 local Job = require("plenary.job")
 
--- get plugin directory path
+-- define path to knitting scripts
 local plugin_path = debug.getinfo(1).source:sub(2):match("(.*/).*/")
 local pdf_script_path = plugin_path .. "scripts/knitr-pdf.sh"
 local html_script_path = plugin_path .. "scripts/knitr-html.sh"
 
--- knit file to pdf
-K.knitr_pdf = function()
-    local filename = vim.fn.expand("%:t")
-
-    -- check if current file is an R file
-    if filename:match("%.R$") or filename:match("%.Rmd$") then
-        print("Knitting " .. filename .. " to pdf...")
-
-        -- get path to file open in current buffer
-        local filepath = vim.fn.expand("%:p")
-
-        -- run script in background shell process
-        Job:new({
-            command = pdf_script_path,
-            args = { filepath },
-            on_exit = function(_, return_val)
-                if (return_val == 0) then
-                    print("Knitting finished sucessfully.")
-                else
-                    print("Knitting failed with exit code: " .. return_val)
-                end
-            end,
-        }):start()
-
-    else
-        print("Current file is not a .R or .Rmd file.")
-    end
-
+-- display to message history
+local function display_messages(messages)
+    vim.schedule(function()
+        local concatenated_messages = table.concat(messages, "\n")
+        vim.api.nvim_echo({{concatenated_messages, "None"}}, true, {})
+    end)
 end
 
--- knit file to html
-K.knitr_html= function()
-    local filename = vim.fn.expand("%:t")
-
-    -- check if current file is an R file
-    if filename:match("%.R$") or filename:match("%.Rmd$") then
-
-        -- get path to file open in current buffer
-        local filepath = vim.fn.expand('%:p')
-        print("Knitting " .. filepath .. " to html...")
-
-        -- run script in background shell process
-        Job:new({
-            command = html_script_path,
-            args = { filepath },
-            on_exit = function(_, return_val)
-                if (return_val == 0) then
-                    print("Knitting finished sucessfully.")
-                else
-                    print("Knitting failed with exit code: " .. return_val)
-                end
-            end,
-        }):start()
-
+-- handle job errors and output
+local function handle_job_output(err, return_val)
+    local messages = {}
+    if return_val == 0 then
+        table.insert(messages, "Knitting finished successfully.")
     else
-        print("Current file is not a .R or .Rmd file.")
+        table.insert(messages, "Knitting failed with exit code: " .. return_val)
+        if err and #err > 0 then
+            table.insert(messages, "Error Trace:")
+            for _, line in ipairs(err) do
+                table.insert(messages, line)
+            end
+        end
     end
+    display_messages(messages)
 end
 
--- Create user commands
-vim.api.nvim_create_user_command("KnitRpdf", K.knitr_pdf, {})
-vim.api.nvim_create_user_command("KnitRhtml", K.knitr_html, {})
+function K.knitr_pdf()
+    local filename = vim.fn.expand("%:t")
+    if not (filename:match("%.R$") or filename:match("%.Rmd$")) then
+        print("Current file is not a .R or .Rmd file.")
+        return
+    end
+
+    print("Knitting " .. filename .. " to pdf...")
+    local filepath = vim.fn.expand("%:p")
+    local err_output = {}
+
+    Job:new({
+        command = pdf_script_path,
+        args = {filepath},
+        on_stderr = function(_, data)
+            table.insert(err_output, data)
+        end,
+        on_exit = function(_, return_val)
+            handle_job_output(err_output, return_val)
+        end,
+    }):start()
+end
+
+function K.knitr_html()
+    local filename = vim.fn.expand("%:t")
+    if not (filename:match("%.R$") or filename:match("%.Rmd$")) then
+        print("Current file is not a .R or .Rmd file.")
+        return
+    end
+
+    print("Knitting " .. filename .. " to html...")
+    local filepath = vim.fn.expand("%:p")
+    local err_output = {}
+
+    Job:new({
+        command = html_script_path,
+        args = {filepath},
+        on_stderr = function(_, data)
+            table.insert(err_output, data)
+        end,
+        on_exit = function(_, return_val)
+            handle_job_output(err_output, return_val)
+        end,
+    }):start()
+end
+
+-- create user commands upon setup
+function K.setup()
+    vim.api.nvim_create_user_command("KnitRpdf", K.knitr_pdf, {
+        desc = "Knit current R or Rmd file to PDF"
+    })
+    vim.api.nvim_create_user_command("KnitRhtml", K.knitr_html, {
+        desc = "Knit current R or Rmd file to HTML"
+    })
+end
 
 return K
